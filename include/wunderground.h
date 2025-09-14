749b3f57dda7cbfb72c5826e5e5b6d7b4e0fe206
TιJ <78140568+TiJ-code@users.noreply.github.com>
2025-09-14 13:24:37 +0200

    Added Client Datatype
    Added client for Wunderground API with constructor and deconstructors.

--- include/wunderground.h
+++ include/wunderground.h
@@ -4,4 +4,12 @@
 int wu_global_init(void);
 void wu_global_cleanup(void);
 
+typedef struct {
+    const char* api_key;
+    const char* base_url;
+} wu_client_t;
+
+wu_client_t* wu_client_new(const char* api_key);
+void wu_client_delete(wu_client_t* client);
+
 #endif //CWUNDERGROUNDAPI_WUNDERGROUND_H
--- src/wunderground.c
+++ src/wunderground.c
@@ -3,10 +3,30 @@
 
 #include "../include/wunderground.h"
 
+#include <stdlib.h>
+
 int wu_global_init(void) {
     return curl_global_init(CURL_GLOBAL_DEFAULT);
 }
 
 void wu_global_cleanup(void) {
     curl_global_cleanup();
+}
+
+wu_client_t* wu_client_new(const char* api_key) {
+    wu_client_t* client = (wu_client_t*) malloc(sizeof(wu_client_t));
+    if (!client) return NULL;
+
+    client->api_key = api_key;
+    client->base_url = "https://wunderground.com/api/";
+
+    return client;
+}
+
+void wu_client_free(wu_client_t* client) {
+    if (!client) return;
+
+    free(client->api_key);
+    free(client->base_url);
+    free(client);
 }
